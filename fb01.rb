ba569ee62a0f8a77ed5695e6d0c9f8a281308b8d
Koreharu <kurakore112@gmail.com>
2016-11-08 21:18:16 +0900

    delete private data

--- fb01.rb
+++ fb01.rb
@@ -1,10 +1,10 @@
 require'twitter'
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "UntXYTPmbao0Ns10VqgsfQjU1"
-  config.consumer_secret     = "H3widALbyQUUJtoigDHV6yDPSUm2IavMGAjJP8248IpKPlsFqe"
-  config.access_token        = "313283891-1NtKrBfXLfHcmGJVUXsIgkfWl4Qs0NM7C1QpT0az"
-  config.access_token_secret = "c50Fpc8yu2YHVsuYjhpiRK7z66oPAjpzoAtrCI9EzgTDJ"
+  config.consumer_key        = ""
+  config.consumer_secret     = ""
+  config.access_token        = ""
+  config.access_token_secret = ""
 end
 
 index = client.follower_ids("kurakore_index").to_a
